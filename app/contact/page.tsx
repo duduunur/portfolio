548989d3fa72604c1818878b0ee9a978f5a1c6fa
Dudunur Isik <dudunur.isik@uni-wuppertal.de>
2024-06-06 11:01:49 +0200

    updated: metadata, contact page, footer, navbar hover effekt

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,9 +8,19 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Kontakt</h1>
-      <p className="mb-4">
+       {/*<a href="mailto:hello@dudunurisik.de">
+        <p className="mb-4">
         {`hello@dudunurisik.de`}
       </p>
+  </a>*/}
+  <a
+        className="flex items-center transition-all hover:text-neutral-700 dark:hover:text-neutral-300"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="mailto:hello@dudunurisik.de"
+      >
+        <p className="ml-0 h-7">hello@dudunurisik.de</p>
+      </a>
     </section>
   )
 }
