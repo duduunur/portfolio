f1eb2da39b6a5a2d67194beaa3d0de1daf8cff9d
Dudunur Isik <dudunur.isik@uni-wuppertal.de>
2024-06-07 11:16:51 +0200

    added home page title and description

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        this website is under construction (´∩｡• ᵕ •｡∩`)♡
+        Dudunur Isik
       </h1>
       <p className="mb-4">
-        {/*`Studentin der Teilstudiengänge Informatik & Design Interaktiver Medien an der Bergischen Universität Wuppertal`*/}
+        {`Studentin der Teilstudiengänge Informatik & Design Interaktiver Medien an der Bergischen Universität Wuppertal`}
       </p>
-      {/*
+      {
       <div className="my-8">
         <Projects />
-      </div>*/}
+      </div>}
     </section>
   )
 }
