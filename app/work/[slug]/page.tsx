9bb71ffcee9b96c190e05bd7b8e8ed491a131d2b
Dudunur Isik <dudunur.isik@uni-wuppertal.de>
2024-06-11 16:30:56 +0200

    trying to disable hover effect on mobile

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -1,7 +1,23 @@
 import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
-import { /*formatDate,*/ getProjects } from 'app/work/utils'
+import { getProjects } from 'app/work/utils'
 import { baseUrl } from 'app/sitemap'
+
+function ArrowIcon() {
+  return (
+    <svg
+    width="15"
+    height="15"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+d="M9.92898 11.3494L11.0369 10.2415L2.7983 1.98864H9.16193L9.14773 0.454545H0.15625V9.46023H1.70455L1.69035 3.09659L9.92898 11.3494Z"      fill="currentColor"
+    />
+  </svg>
+  );
+}
 
 export async function generateStaticParams() {
   let projects = getProjects()
@@ -57,39 +73,16 @@ export default function Work({ params }) {
   }
 
   return (
-    <section> {/*
-      <script
-        type="application/ld+json"
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'Work',
-            headline: project.metadata.title,
-            datePublished: project.metadata.publishedAt,
-            dateModified: post.metadata.publishedAt,
-            description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${baseUrl}${post.metadata.image}`
-              : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/work/${post.slug}`,
-            author: {
-              '@type': 'Person',
-              name: 'My Portfolio',
-            },
-          }),
-        }}
-      />*/}
-      <h1 className="title font-semibold text-2xl tracking-tighter">
+    <section>
+          <a
+            className="transition-all hover:text-neutral-700 dark:hover:text-neutral-100"
+            href="/"
+          >
+            <ArrowIcon/>
+          </a>
+      <h1 className="mt-2 title font-medium text-2xl tracking-tighter">
         {project.metadata.title}
       </h1>
-      {/*
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {formatDate(project.metadata.publishedAt)}
-        </p>
-      </div>
-    */}
       <article className="prose">
         <CustomMDX source={project.content} />
       </article>
