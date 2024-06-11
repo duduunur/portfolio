3fbb73e1bc7122995880982c789ad01fc2152ce8
Dudunur Isik <dudunur.isik@uni-wuppertal.de>
2024-06-11 16:57:08 +0200

    another try, this time using the tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'betterhover': {'raw': '(hover: hover)'},
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
