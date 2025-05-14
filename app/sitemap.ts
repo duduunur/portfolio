6d40f1c161d26fa46b19f0dae7254d19fdc119d4
Dudunur Isik <dudunur.isik@uni-wuppertal.de>
2025-05-14 17:13:50 +0200

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,10 @@
-import { getProjects } from 'app/work/utils'
-
 export const baseUrl = 'https://dudunurisik.de'
 
 export default async function sitemap() {
-  let projects = getProjects().map((project) => ({
-    url: `${baseUrl}/work/${project.slug}`,
-    lastModified: project.metadata.publishedAt,
-  }))
-
-  let routes = ['', '/work'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
-
-  return [...routes, ...projects]
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date().toISOString().split('T')[0],
+    },
+  ]
 }
